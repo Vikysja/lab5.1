b01f49e9653756e028e3d15af9c84fe3f33f1a25
Vikysja <91226103+Vikysja@users.noreply.github.com>
2021-11-03 21:27:22 +0200

    Add files via upload

---
+++ lab5.1.cpp
@@ -0,0 +1,27 @@
+﻿// lab5.1.cpp
+// Квецко Вікторія
+// Лабораторна робота № 5.1
+// Функції, що містять арифметичний вираз
+// Варіант 11
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(const double a, const double b); 
+
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c =g(1,s)+pow(1+pow(g(t,1),2),2)/(1+pow(g(s+t,1),3)) ;
+	cout << "c = " << c << endl;
+	return 0;
+}
+double g(const double a, const double b)
+{
+	return pow(a,2)+ a*b+pow(b,2);
+}
